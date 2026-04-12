5c7173462e66d8bbd9ab1b7d07ed20b993ffa2aa
SamuelHenrique007 <sh0161663@gmail.com>
2026-04-12 03:15:52 -0300

    Correções

--- focusflow_render/backend/config/settings.py
+++ focusflow_render/backend/config/settings.py
@@ -9,7 +9,6 @@ from datetime import timedelta
 from pathlib import Path
 
 import dj_database_url
-from django.contrib.auth import get_user_model
 from django.utils.translation import gettext_lazy as _
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -202,15 +201,3 @@ SECURE_HSTS_PRELOAD = env_bool("SECURE_HSTS_PRELOAD", default=not DEBUG)
 
 
 
-if os.getenv("CREATE_SUPERUSER") == "true":
-    User = get_user_model()
-    email = os.getenv("SUPERUSER_EMAIL")
-    password = os.getenv("SUPERUSER_PASSWORD")
-
-    if email and password and not User.objects.filter(email=email).exists():
-        User.objects.create_superuser(
-            username=email,
-            email=email,
-            password=password,
-            name="Administrador",
-        )
