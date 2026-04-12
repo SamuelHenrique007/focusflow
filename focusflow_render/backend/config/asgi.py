3c63bc9259da1add656143a5c5c41709b698dc4d
SamuelHenrique007 <sh0161663@gmail.com>
2026-04-12 02:05:27 -0300

    correção asgi.py

--- focusflow_render/backend/config/asgi.py
+++ focusflow_render/backend/config/asgi.py
@@ -5,10 +5,10 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 
+django_asgi_app = get_asgi_application()
+
 from notifications.routing import websocket_urlpatterns
 from notifications.auth import JwtAuthMiddlewareStack
-
-django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
